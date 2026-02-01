fd78c9657e0e7e40791d807a1d42fd6c9997d456
erinskieasy <xx876xx@gmail.com>
2026-02-01 00:43:38 -0500

    Fixing Express 5 Error: You are correct, the logs show a PathError. This is because we are using Express v5, where the * wildcard syntax changed.
    I updated server/index.ts to use (.*) which is the correct syntax for "catch all" in the new version.

--- server/index.ts
+++ server/index.ts
@@ -167,7 +167,7 @@ if (isProduction) {
     console.log('Serving static files from ../dist');
     app.use(express.static(path.join(__dirname, '../dist')))
 
-    app.get('*', (req, res) => {
+    app.get('(.*)', (req, res) => {
         res.sendFile(path.join(__dirname, '../dist/index.html'))
     })
 }
